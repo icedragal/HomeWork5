95ebb9cc9e3c0db6ee41fbcb702067fdd188c0cd
Icedragal <icedragal@gmail.com>
2022-11-06 13:52:52 +0300

    Задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-
+        //Задание 1
+        System.out.println("Задание 1");
+        char clientOS = 0;
+        if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (clientOS == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+        else {
+            System.out.println("Ошибка определения ОС");
+        }
     }
 }
