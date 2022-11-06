5d5051e99fa6d54e4b33e16e42377a182a06aa19
Icedragal <icedragal@gmail.com>
2022-11-06 14:36:26 +0300

    Задание 2

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         //Задание 1
         System.out.println("Задание 1");
-        char clientOS = 0;
+        char clientOS = 1;
         if (clientOS == 0) {
             System.out.println("Установите версию приложения для iOS по ссылке");
         } else if (clientOS == 1) {
@@ -11,5 +11,28 @@ public class Main {
         else {
             System.out.println("Ошибка определения ОС");
         }
+
+        //Задание 2
+        System.out.println("Задание 2");
+        int clientDeviceYear = 2015;
+        if (clientOS == 0) {
+            if (clientDeviceYear < 2015){
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            }
+            else {
+                System.out.println("Установите версию приложения для iOS по ссылке");
+            }
+        } else if (clientOS == 1) {
+            if ( clientDeviceYear < 2015){
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            }
+            else {
+                System.out.println("Установите версию приложения для Android по ссылке");
+            }
+        }
+        else {
+            System.out.println("Ошибка определения ОС");
+        }
+
     }
 }
